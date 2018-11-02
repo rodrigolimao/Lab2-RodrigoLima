898fb919b96bb4e847e2b6e3b43196fd13555b3d
Rodrigo <rodrigoclimao@gmail.com>
2018-11-02 14:30:26 -0400

    barchart

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,5 +1,7 @@
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 import java.io.File;
@@ -11,9 +13,13 @@ import javafx.scene.image.Image;
 public class DBConnect {
     private static String userName = "student";
     private static String password = "student";
-    private XYChart.Series movieSeriesMale;
+    private XYChart.Series salesSeries;
     @FXML
     private BarChart<?, ?> barChart;
+    @FXML
+    private CategoryAxis months;
+    @FXML
+    private NumberAxis sales;
 
 
     public static ArrayList<Speaker> getAllSpeakers() throws SQLException {
@@ -22,7 +28,7 @@ public class DBConnect {
         Statement statement = null;
         ResultSet rs = null;
 
-        try{
+        try {
             //1. connect to the database
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/COMP1011T2S1?useSSL=false",
                     userName, password);
@@ -34,24 +40,19 @@ public class DBConnect {
             rs = statement.executeQuery("SELECT * FROM speakers");
 
             //4.  loop over the results and add to the ArrayList
-            while (rs.next())
-            {
+            while (rs.next()) {
                 Speaker newSpeaker = new Speaker(rs.getInt("speakerID"),
                         rs.getString("productName"),
-                       rs.getString("description"),
+                        rs.getString("description"),
                         rs.getDouble("price"));
-                        newSpeaker.setImage(new Image(rs.getString("image")));
+                newSpeaker.setImage(new Image(rs.getString("image")));
 
                 speakers.add(newSpeaker);
             }
 
-
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.err.println(e);
-        }
-        finally {
+        } finally {
             if (conn != null)
                 conn.close();
             if (statement != null)
@@ -64,22 +65,46 @@ public class DBConnect {
     }
 
 
-//    public void graph(int speakerID) {
-//        movieSeriesMale = new XYChart.Series();
-//        barChart.setTitle("Sales for 2018");
-//        movies.setLabel("movie type");
-//        students.setLabel("# of students");
-//
-//        //add data to the the data series
-//        movieSeriesMale.getData().add(new XYChart.Data("Comedy", 10));
-//        movieSeriesMale.getData().add(new XYChart.Data("Action", 15));
-//        movieSeriesMale.getData().add(new XYChart.Data("Romance", 5));
-//        movieSeriesMale.getData().add(new XYChart.Data("Thriller", 9));
-//        movieSeriesMale.setName("Male");
-//
-//        //add the data series to the bar chart
-//        barChart.getData().addAll(movieSeriesMale);
-//
-//    }
+    public void graph(int speakerID) throws SQLException {
+        salesSeries = new XYChart.Series();
+        barChart.setTitle("Sales for 2018");
+        months.setLabel("movie type");
+        sales.setLabel("# of sales");
+
+        ArrayList<Speaker> sales = new ArrayList<>();
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        try {
+            //1. connect to the database
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/COMP1011T2S1?useSSL=false",
+                    userName, password);
+
+            //2.  Create a statement object
+            statement = conn.createStatement();
+
+            //3.  create and execute the query
+            rs = statement.executeQuery("SELECT * FROM sales");
+
+            //4.  loop over the results and add to the data series
+            while (rs.next()) {
+                salesSeries.getData().add(new XYChart.Data(rs.getInt("speakerID"),rs.getInt("speunitsSold")));
+
+                //add the data series to the bar chart
+                barChart.getData().addAll(salesSeries);
+            }
+
+        } catch (SQLException e) {
+        System.err.println(e);
+    } finally {
+        if (conn != null)
+            conn.close();
+        if (statement != null)
+            statement.close();
+        if (rs != null)
+            rs.close();
+    }
 
     }
+}
